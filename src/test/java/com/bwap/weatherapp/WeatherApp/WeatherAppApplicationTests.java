992ed77cf1c7fd2ed16a462136f8908cc9c77c4d
robin bisht <49743940+robinsinghBisht@users.noreply.github.com>
2024-11-19 20:10:02 +0530

    Update WeatherAppApplicationTests.java

--- src/test/java/com/bwap/weatherapp/WeatherApp/WeatherAppApplicationTests.java
+++ src/test/java/com/bwap/weatherapp/WeatherApp/WeatherAppApplicationTests.java
@@ -1,16 +1,16 @@
-package com.bwap.weatherapp.WeatherApp;
+// package com.bwap.weatherapp.WeatherApp;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class WeatherAppApplicationTests {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class WeatherAppApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+// 	@Test
+// 	public void contextLoads() {
+// 	}
 
-}
+// }
